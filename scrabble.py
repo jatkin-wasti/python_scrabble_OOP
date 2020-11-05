c40a28254f6a85d1f93925fb7ff6ad4093649219
jatkin-wasti <jatkin-wasti@spartaglobal.com>
2020-11-05 19:02:14 +0000

    added appropriate comments throughout both files

--- scrabble.py
+++ scrabble.py
@@ -1,5 +1,6 @@
-class ScrabbleDict:
+class ScrabbleDict:  # Creating the class that will hold the letters and their values
     def __init__(self):
+        # We can iterate through these lists and assign relevant scores in our other class
         self.one_point = ["a", "e", "i", "o", "u", "l", "n", "r", "s", "t"]
         self.two_points = ["d", "g"]
         self.three_points = ["b", "c", "m", "p"]
@@ -7,4 +8,3 @@ class ScrabbleDict:
         self.five_points = ["k"]
         self.eight_points = ["j", "x"]
         self.ten_points = ["q", "z"]
-
--- scrabble_calculator.py
+++ scrabble_calculator.py
@@ -1,14 +1,14 @@
-from scrabble import ScrabbleDict
+from scrabble import ScrabbleDict  # First we need to import the ScrabbleDict class
 
 
-class ScrabbleCalculator(ScrabbleDict):
-    def __init__(self, word):
-        super().__init__()
-        self.score = 0
-        self.word = word.lower()
+class ScrabbleCalculator(ScrabbleDict):  # Inheriting from the Parent class
+    def __init__(self, word):  # Takes a word argument to calculate its score
+        super().__init__()  # Using the parent classes initialisation
+        self.score = 0  # Set the score to 0 to start with
+        self.word = word.lower()  # using .lower() saves us having to check for capitals and lowercase in the dict
 
     def score_check(self):
-        for letters in self.word:
+        for letters in self.word:  # Loops through the word and increments the value with the appropriate score
             if letters in self.one_point:
                 self.score += 1
             elif letters in self.two_points:
@@ -23,10 +23,12 @@ class ScrabbleCalculator(ScrabbleDict):
                 self.score += 8
             elif letters in self.ten_points:
                 self.score += 10
-            else:
-                return "Please only include letters!"
-        return self.score
+            else:  # If the character received wasn't found in any of the dictionaries it must not be a letter
+                return "Please only include letters!"  # So give the user some feedback
+        return self.score  # Return the calculated score
 
 
-test = ScrabbleCalculator("Amazing")
+test = ScrabbleCalculator("Amazing")  # Testing for an expected input
 print(test.score_check())
+test2 = ScrabbleCalculator("aglamgl!26262")  # Testing for an unexpected input
+print(test2.score_check())
